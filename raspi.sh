870c925a8e3eddc40cfb318dd8b177e064a5f9e3
Lumpi75 <lumkek@me.com>
2025-04-14 10:46:35 +0200

    Update raspi.sh

--- raspi.sh
+++ raspi.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Farben definieren
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
@@ -9,14 +8,12 @@ NC='\033[0m' # No Color
 set -e
 clear
 
-# Eigener Skriptordner erkennen
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 echo -e "${GREEN}--------------------------------------${NC}"
 echo -e "${YELLOW}🛠️  Raspberry Pi Installationsmenü${NC}"
 echo -e "${GREEN}--------------------------------------${NC}"
 
-# Funktionen
 show_menu() {
     echo ""
     echo "Was möchtest du tun?"
