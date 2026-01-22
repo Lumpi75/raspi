4cdfaf56430b4838bf499a665b222b5ddde5756b
Lumpi75 <lumkek@me.com>
2026-01-22 13:00:26 +0100

    Update Install-Tools.sh
    Tilde update

--- Install-Tools.sh
+++ Install-Tools.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-tools=("git" "tilde" "htop" "curl" "python3-pip")
+
+tools=("git" "htop" "curl" "python3-pip")
+
+echo ">> Paketliste aktualisieren"
+sudo apt update
+
 for tool in "${tools[@]}"; do
     read -p "$tool installieren? (Y/n): " answer
     case "$answer" in
-        [Yy]* | "" ) sudo apt install -y $tool && echo -e "\033[0;32m✅ $tool installiert.\033[0m" ;;
-        [Nn]* ) echo -e "\033[1;33m➡️  $tool übersprungen.\033[0m" ;;
-        * ) echo -e "\033[0;31m❌ Ungültige Eingabe. $tool übersprungen.\033[0m" ;;
+        [Yy]* | "" )
+            sudo apt install -y "$tool" \
+              && echo -e "\033[0;32m✅ $tool installiert.\033[0m"
+            ;;
+        [Nn]* )
+            echo -e "\033[1;33m➡️  $tool übersprungen.\033[0m"
+            ;;
+        * )
+            echo -e "\033[0;31m❌ Ungültige Eingabe. $tool übersprungen.\033[0m"
+            ;;
     esac
 done
+
 read -p "Weiter mit [Enter]..."
